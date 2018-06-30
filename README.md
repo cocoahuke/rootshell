59acde521726dd7d37ed31058112cd074aca190e
CocoaHuke <cocoatouch@outlook.com>
2018-06-29 23:00:52 -0700

    Update

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-#rootshell
+# rootshell
+- Simple wrap of **sudo**
+- Carry current user environment variables to root
+- Keep user's home directory
+```
+bash-3.2$ export USER_ENV="test_value"
+bash-3.2$ rootshell
+rootshell-3.2# echo $USER_ENV
+test_value
+rootshell-3.2# echo ~
+/Users/user
+rootshell-3.2#
+```
 
+<<<<<<< HEAD
+Tested on MacOS
+=======
 ###simple wrap of sudo login without password everytime
 ![rootshell_icon](demo.gif)
 
 test in Macos/linux debian
+>>>>>>> origin/master
--- rootshell.c
+++ rootshell.c
@@ -7,26 +7,48 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[], const char *envp[]) {
+    
+    uint32_t envp_count = 0;
+    int32_t envp_ins = -1;
+    const char **env = envp;
+    while(*env){
+        if(!strcmp(*env, "PS1"))
+            envp_ins = envp_count;
+        envp_count++, env++;
+    }
+    
+    void *envp_copy = malloc((envp_count + ((envp_ins!=-1)?1:2)) * sizeof(envp[0]));
+    bzero(envp_copy, (envp_count + ((envp_ins!=-1)?1:2)) * sizeof(envp[0]));
+    memcpy(envp_copy, envp, (envp_count+1) * sizeof(envp[0]));
+    
+    if(envp_ins != -1)
+        ((typeof(envp))envp_copy)[envp_ins] = "PS1=rootshell-3.2# ";
+    else
+        ((typeof(envp))envp_copy)[envp_count++] = "PS1=rootshell-3.2# ";
+    
     setuid(0);
     if(argc==1){
-        setenv("PS1","rootshell-3.2# ",1);
-        char *shargv[]={"/usr/bin/sudo","-E","/bin/sh",NULL};
-        execv("/usr/bin/sudo",shargv);
+        envp_copy = realloc(envp_copy, (envp_count + 2 + 1) * sizeof(envp[0]));
+        memmove(envp_copy + sizeof(envp[0]), envp_copy, envp_count * sizeof(envp[0]));
+        ((typeof(envp))envp_copy)[0] = "/usr/bin/sudo";
+        ((typeof(envp))envp_copy)[envp_count+1] = "/bin/bash";
+        ((typeof(envp))envp_copy)[envp_count+2] = NULL;
+        
+        execv("/usr/bin/sudo", envp_copy);
     }
     else{
-        char *shargv[argc+2];
-        for(int i=0;i<argc;i++){
-            shargv[i+1] = (char *)argv[i];
-        }
-        shargv[0] = "/usr/bin/sudo";
-        shargv[1] = "-E";
-        shargv[argc+1] = NULL;
-
-        execv("/usr/bin/sudo",shargv);
+        envp_copy = realloc(envp_copy, (envp_count + argc + 1) * sizeof(envp[0]));
+        memmove(envp_copy + sizeof(envp[0]), envp_copy, envp_count * sizeof(envp[0]));
+        ((typeof(envp))envp_copy)[0] = "/usr/bin/sudo";
+        memcpy(&((typeof(envp))envp_copy)[envp_count+1], &argv[1], (argc-1)*sizeof(envp[0]));
+        ((typeof(envp))envp_copy)[envp_count + argc] = NULL;
+        
+        execv("/usr/bin/sudo", envp_copy);
     }
     return 0;
 }
